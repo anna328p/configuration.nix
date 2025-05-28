6786fbda37fa29868ac4d6f09751c7a99c1aff47
Anna Kudriavtsev <anna328p@gmail.com>
2025-05-28 15:12:58 -0500

    Add keepassxc to autostart

--- home/workstation/programs.nix
+++ home/workstation/programs.nix
@@ -10,12 +10,20 @@
         gpg-agent = {
             enable = true;
             enableSshSupport = true;
-            pinentryPackage = pkgs.pinentry-gnome3;
+            pinentry.package = pkgs.pinentry-gnome3;
         };
     };
 
     programs = {
         gpg.enable = true;
         obs-studio.enable = systemConfig.misc.buildFull;
+    };
+
+    xdg.autostart = {
+        enable = true;
+
+        entries = [
+            "/run/current-system/sw/share/applications/org.keepassxc.KeePassXC.desktop"
+        ];
     };
 }
